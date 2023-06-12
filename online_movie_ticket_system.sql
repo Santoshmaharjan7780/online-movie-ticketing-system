1706ada46231062edcf2b8d73e2a6fc5e4b0cc95
Santosh Maharjan <131969485+Santoshmaharjan7780@users.noreply.github.com>
2023-06-12 08:04:51 +0100

    Update online_movie_ticket_system.sql

--- online_movie_ticket_system.sql
+++ online_movie_ticket_system.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pizza_cutter`
+-- Database: `online_movie_ticketing_system`
 --
 
 -- --------------------------------------------------------
